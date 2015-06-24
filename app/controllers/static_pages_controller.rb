5700f8444160b37adc1320f9c4cd23d8195f5ba5
Hubert Olender <olender.hub@gmail.com>
2015-06-24 23:58:15 +0200

    test27

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,23 +1,16 @@
 class StaticPagesController < ApplicationController
 CONN = ActiveRecord::Base.connection
+
   def home
 
     @auth_with_ownapphub = Koala::Facebook::OAuth.new(867013733372006, "ff8935cc1a374399d4493a3d2fa60c84")
-    @token2 = @auth_with_ownapphub.get_app_access_token
+    @token = @auth_with_ownapphub.get_app_access_token
 
-
-  	@token = "CAACEdEose0cBACA8smnnmnw32G9adobVnWtrZCIA5UzEz3SFdr4ZCjZAux9dT9mWfLV06GdSe8dIKCzQxZCcZB4uxkWIVZAJtjKyO7uKo2jMy9ZB7QMWE9rwuxuujVCNLE4fJ5ASaaLaj53GoMqamNqWRm0l2hYV87J1BitBZC51FsARMZAuZBvL4Nx07i4w2gsH4tod8pqEu8mIDOS9lVu6ivxq5DdxM6hX8ZD"
-  	info = Koala::Facebook::API.new(@token2)
+  	info = Koala::Facebook::API.new(@token)
     @getinfo1 = info.get_connections(id="270028826443166", connection_name="feed?fields=id,description,message,from,picture,comments", {:limit => 10})
     @getall = WroclawPokojeWynajem.search(params)
 
-
-
-
-
     # @getall = WroclawPokojeWynajem.all.sort
-
-
 
 # zapis do bazy danych rekordy z facebook
     # @getinfo1.each_with_index do |abc, index|
@@ -42,56 +35,55 @@ CONN = ActiveRecord::Base.connection
     # end
 # zapis do bazy danych rekordy z facebook
 
-
 # zapis do bazy danych rekordy z facebook druga metoda
-@userphoto = []
-@username = []
-@adsphoto = []
-@description1 = []
-@description2 = []
-@linktooffer = []
-@updatedinfacebok = []
-@number_of_comments = []
+    @userphoto = []
+    @username = []
+    @adsphoto = []
+    @description1 = []
+    @description2 = []
+    @linktooffer = []
+    @updatedinfacebok = []
+    @number_of_comments = []
 
-@getinfo1.each do |abc|
-  @userphoto << "https://graph.facebook.com/" + "#{abc["from"]["id"]}" + "/picture?type=square"
-  @username << abc["from"]["name"]
-  @adsphoto << "#{abc["picture"]}"
-  @description1 << ( if abc["message"] != nil
-                       abc["message"].gsub("'", "")
-                     else
-                       nil
-                     end
-                   )
-  @description2 << (if abc["description"] != nil
-                      abc["description"]
-                    else
-                      nil
-                    end
-                   )
-  @linktooffer << "http://facebook.pl/" + "#{abc["id"]}"
-  @updatedinfacebok << abc["updated_time"].to_time.strftime("%F %T")
-  @number_of_comments << ( if abc["comments"] != nil
-                             abc["comments"]["data"].count
-                           else
-                             0
-                           end
-                         )
-end
+    @getinfo1.each do |abc|
+      @userphoto << "https://graph.facebook.com/" + "#{abc["from"]["id"]}" + "/picture?type=square"
+      @username << abc["from"]["name"]
+      @adsphoto << "#{abc["picture"]}"
+      @description1 << ( if abc["message"] != nil
+                           abc["message"].gsub("'", "")
+                         else
+                           nil
+                         end
+                       )
+      @description2 << (if abc["description"] != nil
+                          abc["description"]
+                        else
+                          nil
+                        end
+                       )
+      @linktooffer << "http://facebook.pl/" + "#{abc["id"]}"
+      @updatedinfacebok << abc["updated_time"].to_time.strftime("%F %T")
+      @number_of_comments << ( if abc["comments"] != nil
+                                 abc["comments"]["data"].count
+                               else
+                                 0
+                               end
+                             )
+    end
 
-inserts = (1..10).to_a
-inserts.each_with_index do |abc, index|
-  sql = "UPDATE wroclaw_pokoje_wynajems SET userphoto='#{@userphoto[index]}',
-                                            username='#{@username[index]}',
-                                            adsphoto='#{@adsphoto[index]}',
-                                            description1='#{@description1[index]}',
-                                            description2='#{@description2[index]}',
-                                            linktooffer='#{@linktooffer[index]}',
-                                            updatedinfacebok='#{@updatedinfacebok[index]}',
-                                            number_of_comments='#{@number_of_comments[index]}'
-                                                                               WHERE id=#{index+1}"
-CONN.execute sql
-end
+    inserts = (1..10).to_a
+    inserts.each_with_index do |abc, index|
+      sql = "UPDATE wroclaw_pokoje_wynajems SET userphoto='#{@userphoto[index]}',
+                                                username='#{@username[index]}',
+                                                adsphoto='#{@adsphoto[index]}',
+                                                description1='#{@description1[index]}',
+                                                description2='#{@description2[index]}',
+                                                linktooffer='#{@linktooffer[index]}',
+                                                updatedinfacebok='#{@updatedinfacebok[index]}',
+                                                number_of_comments='#{@number_of_comments[index]}'
+                                                WHERE id=#{index+1}"
+      CONN.execute sql
+    end
 # zapis do bazy danych rekordy z facebook druga metoda
   end
 end
